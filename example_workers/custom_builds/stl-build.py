2ac5265ff3a8a7678be64cc327ffb8d7355311bf
BitterKanegul <15769643+BitterKanegul@users.noreply.github.com>
2024-09-15 21:17:45 -0400

    Update stl-build.py

--- example_workers/custom_builds/stl-build.py
+++ example_workers/custom_builds/stl-build.py
@@ -27,17 +27,17 @@ def build_hook(build_dir,
                           f"cmake --preset {arch_map[arch]} &&" +
                           f"cmake --build --preset {arch_map[arch]}", platform="windows", cwd=clone_dir, timelimit=600000)
 
-
+arch = 'x86'
 
 obj = CustomWindowsBuild(clone_dir='C:\\\\STL\\',
                          clone_flags=' --recursive',
                          collect_dir="C:\\Binaries",
                          source_dir='C:\\\\STL\\',
-                         build_dir='C:\\\\STL\\',
+                         build_dir=f'C:\\\\STL\\out\\{arch}\\out',
                          project_git_url='https://github.com/microsoft/STL',
                          optimization='',
                          build_mode='Debug',
-                         arch='x86',
+                         arch=arch,
                          tags=['vs-2022-17.11','vs-2022-17.10','vs-2022-17.9','vs-2022-17.8'],
                          build_hook=build_hook,
                          install_prerequisites_hook=install_prerequisites_hook,
